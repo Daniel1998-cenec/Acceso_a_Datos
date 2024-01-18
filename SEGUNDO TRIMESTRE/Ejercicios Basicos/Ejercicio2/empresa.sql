fa28080c6dbe4924612ab8a6ccebcca629ba54d7
Daniel1998-cenec <danielcesar1998@hotmail.com>
2024-01-18 17:08:01 +0100

    Bloque de actividades báasica terminada!!

---
+++ SEGUNDO TRIMESTRE/Ejercicios Basicos/Ejercicio2/empresa.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS `empresa`;
+USE `empresa`;
+
+-- Volcando estructura para tabla empresa.clientes
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Volcando datos para la tabla empresa.clientes: 6 rows
+INSERT INTO `clientes` (`id`, `nombre`, `ciudad`) VALUES
+	(1, 'Alberto', NULL),
+	(2, 'Cristina', NULL),
+	(3, 'Eva', 'Sevilla'),
+	(4, 'Eva', 'Sevilla'),
+	(5, 'Eva', 'Sevilla'),
+	(6, 'Eva', 'Sevilla');
+    
+-- Volcando estructura para procedimiento empresa.ObtenerClientes
+DELIMITER //
+CREATE PROCEDURE `ObtenerClientes`()
+BEGIN SELECT * FROM clientes;END//
+DELIMITER ;
+
+-- Volcando estructura para tabla empresa.tvideojuegos
+CREATE TABLE IF NOT EXISTS `tvideojuegos` (
+  `idjuego` int NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `anio` int NOT NULL,
+  `empresa` varchar(50) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `sinopsis` varchar(50) DEFAULT NULL,
+  `plataforma` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idjuego`)
+);
---
+++ SEGUNDO TRIMESTRE/Ejercicios Basicos/Ejercicio3y7/src/main/java/Modulo/Main.java
@@ -0,0 +1,41 @@
+package Modulo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory();
+		
+		ThreadLocalSessionContext context = new ThreadLocalSessionContext ((SessionFactoryImplementor) sessionFactory);
+				context.bind(sessionFactory.openSession());
+				try {
+					Alumno alumno=new Alumno ("Alberto", "Ruiz", "Rodrigez", Date.valueOf("1975-05-05"), "No", 33333);
+					
+					Session session = context.currentSession();
+					
+					session.beginTransaction();
+					
+					session.save(alumno);
+					
+					session.getTransaction().commit();
+					
+					System.out.println("Alumno: "+alumno);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}finally {
+					ThreadLocalSessionContext.unbind(sessionFactory);
+					sessionFactory.close();
+
+				}
+	}
+
+}
